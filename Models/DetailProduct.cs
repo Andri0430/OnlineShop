bfa32f35d9932627108fb0888bdad0cfab6b3461
Andriansyah <andriansyah0430@gmail.com>
2023-07-22 14:25:33 +0700

    second

---
+++ Models/DetailProduct.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Models
+{
+    public class DetailProduct
+    {
+        public Product Product { get; set; }
+        public string Deskripsi { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public int Stok { get; set; }
+        public string TanggalRilis { get; set; } = string.Empty;
+        public string TanggalKadaluarsa { get; set; } = string.Empty;
+    }
+}
